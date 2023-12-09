dba08c502cfc8b7c6647604d840fc87d63e8cbc2
Viviana <EMAIL.EMAIL.com>
2023-12-09 14:57:12 -0600

    Permissions updated from original

--- Plants-Vs-Zombies-JAR/PlantsVsZombies.sh
+++ Plants-Vs-Zombies-JAR/PlantsVsZombies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LIB_PATH=javafx-sdk-11.0.2/lib/
+export LIB_PATH=javafx-sdk-17.0.9/lib/
 exec java --module-path $LIB_PATH --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.media -jar PlantVsZombies.jar
